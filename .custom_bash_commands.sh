ab83c44a68eb090bca959fe40f496429455d788d
Chris Tregaskis <49287297+ChrisTregaskis@users.noreply.github.com>
2025-06-21 16:47:54 +0100

    pushing a few minor updates

--- .custom_bash_commands.sh
+++ .custom_bash_commands.sh
@@ -3,24 +3,44 @@ function g() {
   case $1 in
   help)
     echo available commands:
-    echo pl = command: git pull origin \[currentBranch]
-    echo ph = command: git push origin HEAD
+    # echo pl = command: git pull origin \[currentBranch]
+    # echo ph = command: git push origin HEAD
+    echo pl = command: git pull
+    echo ph = command: git push
     echo s = command: git status
     echo - = command: git checkout -
     echo c = command: git checkout \$2
     echo nb = command: git checkout -b \$2
+    echo b = command: git branch \-\-sort=-committerdate
+    echo st = command: git stash list
+    echo st_ = command: git stash pop stash@{\$2}
+    echo st__ = command: git stash pop stash@{\0}
+    echo . = command: git add .
+    echo cm = command: git commit -m \$2
   ;;
+
+  # pl)
+  #   branch=$(git symbolic-ref --short HEAD)
+  #   echo pulling branch: "$branch"
+  #   git pull origin "$branch"
+  # ;;
+
+  # ph)
+  #   branch=$(git symbolic-ref --short HEAD)
+  #   echo pushing branch: "$branch"
+  #   git push origin HEAD
+  # ;;
 
   pl)
     branch=$(git symbolic-ref --short HEAD)
     echo pulling branch: "$branch"
-    git pull origin "$branch"
+    git pull
   ;;
 
   ph)
     branch=$(git symbolic-ref --short HEAD)
     echo pushing branch: "$branch"
-    git push origin HEAD
+    git push
   ;;
 
   s)
@@ -35,8 +55,33 @@ function g() {
     git checkout "$2"
   ;;
 
+  b)
+    git branch --sort=-committerdate
+  ;;
+
   nb)
     git checkout -b "$2"
+  ;;
+
+  st)
+    git stash list
+  ;;
+
+  st_)
+    git stash pop stash@{"$2"}
+  ;;
+
+  st__)
+    git stash pop stash@{0}
+  ;;
+
+  .)
+    git add .
+  ;;
+
+  cm)
+    message=$2
+    git commit -m "$message"
   ;;
 
   *)
@@ -54,6 +99,9 @@ function _c() {
     echo id = generates an id to clipboard. Default length "25", accepts requested length.
     echo pass = generates a password to clipboard. Default length "25", accepts requested length.
     echo pin = generates a pin to clipboard. Default length "4", accepts requested length.
+    echo api_ = opens api in vscode
+    echo ui_ = opens ui in vscode OLD - DOES NOT EXIST. Left as example.
+    echo vsc = opens a given folder or file in vscode
     ;;
 
   res)
@@ -79,9 +127,6 @@ function _c() {
     then
       length="$2"
     fi
-    # old working attempt
-    # LC_CTYPE=C -> basically sets language type.
-    # value=$(dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-"$length");
     value=$(LC_ALL=C tr -dc 'A-Za-z0-9' </dev/urandom | head -c "$length" ; echo);
     echo "$value" | pbcopy
     echo copied to clipboard: "$value"
@@ -150,6 +195,15 @@ function _c() {
       echo expecting perc '[command]' followed by "2" conditions, eg _c perc wi "10" "50"
       echo _c perc help = more info
     fi
+  ;;
+
+  # Example of setting up cmd to open common project
+  ui_)
+    open -a "Visual Studio Code" ~/Documents/m/mezze-ui
+  ;;
+
+  vsc)
+    open -a "Visual Studio Code" "$2"
   ;;
 
   *)
